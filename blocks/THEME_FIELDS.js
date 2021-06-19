499d22963944b2f2ab3a28a62b5208f3f1c6158b
Matthew Thomson <matthewthomson.dev@gmail.com>
2021-06-20 09:28:01 +1200

    cd

--- blocks/THEME_FIELDS.js
+++ blocks/THEME_FIELDS.js
@@ -2,28 +2,15 @@ export const THEME_FIELDS = [
     {
       label: "Theme",
       name: "theme.items",
-      component: "group-list",
       itemProps: (item) => ({
         label: item.label,
-      }),
-      defaultItem: () => ({
-        label: "arbitrary",
-        link: "none",
       }),
       onSubmit: () => {
         console.log('saved');
       },
       fields: [
-        {
-          label: "Label",
-          name: "label",
-          component: "text",
-        },
-        {
-          label: "Link",
-          name: "link",
-          component: "text",
-        },
+        { name: "background", label: "Background color", component: "color"},
+        { name: "text", label: "Text color", component: "color"},
       ],
     },
   ];
